3cbeee34303393e80e69d9f934e958716b441752
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-25 22:19:46 +0600

    linear search algorithm with a simple example

---
+++ algorithm/linear_search.js
@@ -0,0 +1,19 @@
+// Linear Search Algorithm/ O(n)
+
+// const friends = ['Nayeem', 'Tumpa', 'Mafruha', 'Urmi', 'Pollob', 'Ekra'];
+
+// for (let i = 0; i < friends.length; i++) {
+//     const element = friends[i];
+//     if (element == 'Mafruha') {
+//         return i;
+//         // console.log(i);
+//     }
+// }
+
+const numbers = [100, 80, 30, 60, 10, 60];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element == 10) {
+        console.log(i);
+    }
+}
