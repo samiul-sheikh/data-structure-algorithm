4200a1d0f933d6938f4bdc1aed23671cf80f9eb6
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-26 22:34:21 +0600

    sorting algorithm bubble sort ascending and descending

---
+++ algorithm/bubble_sort.js
@@ -0,0 +1,43 @@
+// Bubble Sort Algorithm / O(n^2)
+
+// for ascending order
+const numbers = [23, 54, 12, 8, 45, 90, 2];
+// [23, 12, 8, 45, 54, 2, 90];
+// [12, 8, 23, 45, 2, 54, 90];
+// [8, 12, 23, 2, 45, 54, 90];
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+const sorted = bubbleSort(numbers);
+console.log(sorted);
+
+
+
+// for descending order
+const numbers = [25, 95, 23, 45, 67, 15, 30, 56, 18];
+
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] < array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+const sorted = bubbleSort(numbers);
+console.log(sorted);
